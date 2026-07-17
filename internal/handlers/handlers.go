61ca8609ef19b87ec6d4b2214be82849883dd126
Dunaev Alexandr <enklavz@gmail.com>
2026-07-17 14:57:32 +0700

    update handlers after autotests on github

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,7 +12,7 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "../index.html")
+	http.ServeFile(w, r, "index.html")
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
